a00e3925c1ff95d4e6fdeb97f0d2bf634b842da9
Dmitriy <dmitriy.pavlov.17.04@gmail.com>
2022-08-11 14:15:10 +0300

    feat: complete 02-collapse task

--- 02-collapse/index.js
+++ 02-collapse/index.js
@@ -1,1 +1,42 @@
-/* Я комментарий, который ждал тебя здесь. Удали меня и напиши хороший код! */
+const button = document.querySelector('.collapsible__button');
+const buttonTextOnVisibleContent = document.querySelector('.collapsible__action--visible');
+const buttonTextOnHiddenContent = document.querySelector('.collapsible__action--hidden');
+const content = document.querySelector('.collapsible__content');
+
+buttonTextOnVisibleContent.hidden = true;
+
+content.setAttribute('style', `
+    transform: translateY(-40px);
+    opacity: 0;
+    visibility: hidden;
+`)
+
+const contentKeyframeEffects = [
+    { transform: 'translateY(-40px)', opacity: '0', visibility: 'hidden' },
+    { transform: 'translateY(-20px)', opacity: '0', visibility: 'visible' },
+    { transform: 'translateY(-10px)', opacity: '1', visibility: 'visible' },
+    { transform: 'translateY(0px)', opacity: '1', visibility: 'visible'},
+];
+
+const contentAnimationProperties = {
+    duration: 300,
+    easing: 'linear',
+    iterations: 1,
+    fill: 'forwards'
+};
+
+button.addEventListener('click', ()=>{
+    if(buttonTextOnVisibleContent.hidden) {
+        content.animate(contentKeyframeEffects, contentAnimationProperties)
+        
+        buttonTextOnVisibleContent.hidden = false;
+        buttonTextOnHiddenContent.hidden = true;
+    } else {
+        content.animate(contentKeyframeEffects.reverse(), contentAnimationProperties)
+
+        buttonTextOnVisibleContent.hidden = true;
+        buttonTextOnHiddenContent.hidden = false;
+
+        contentKeyframeEffects.reverse()
+    }
+})
